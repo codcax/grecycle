cd3ae3f926d6a660f883e89dac3b6f28889a7237
Tenesh Vignesan <teneshbecs@gmail.com>
2022-02-28 23:10:21 +0800

    Add tailwindcss package and configuration file.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/views/**/*.{html,js,ejs}"],
+    theme: {
+        extend: {
+            colors: {
+                'primary-green': '#3F6A51',
+                'secondary-green': '#4AD19D'
+            },
+        },
+    },
+    plugins: [],
+}
