616ec4bc1ffa37020346652ddb297f52ba589181
Tenesh Vignesan <teneshbecs@gmail.com>
2022-03-02 06:28:25 +0800

    Add shop index page design
    Add dark mode toggle and update design of navigation, shop index page
    content and footer navigation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     content: ["./src/views/**/*.{html,js,ejs}"],
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
                 'primary-green': '#3F6A51',
-                'secondary-green': '#4AD19D'
+                'secondary-green': '#4AD19D',
+                'dark-primary-green': '#081c15',
+                'dark-secondary-green': '#95d5b2'
             },
             fontFamily: {
               'montserrat': ['Montserrat', 'san-serif']
