4a9c1ab1038651d92f441e2524cae27755367bf9
Tenesh Vignesan <teneshdev@gmail.com>
2022-03-06 23:57:47 +0800

    Add login and signup page for shop
    Fix general page designs and add login and signup page.

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -5,3 +5,17 @@ exports.getIndex = (req, res, next) => {
                 path: '/'
             });
 };
+
+exports.getSignUp= (req, res, next) => {
+    res.render("auth/signup", {
+        pageTitle: "Sign Up",
+        path: '/signup'
+    });
+};
+
+exports.getLogin= (req, res, next) => {
+    res.render("auth/login", {
+        pageTitle: "Login",
+        path: '/login'
+    });
+};
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -7,5 +7,7 @@ const shopController = require('../controllers/shop');
 
 
 router.get('/', shopController.getIndex);
+router.get('/signup', shopController.getSignUp);
+router.get('/login', shopController.getLogin);
 
 module.exports = router;
