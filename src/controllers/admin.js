c8df9fdb0ae0f5bf3b7ff247d6410b233fd8f777
Tenesh Vignesan <teneshdev@gmail.com>
2022-03-21 15:14:28 +0800

    Add admin account settings feature and update admin account views.

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -18,7 +18,7 @@ exports.getAdminIndex = (req, res, next) => {
 };
 
 exports.getAdminAccount = (req, res, next) => {
-    const loggedInEmail = req.session.admin.email;
+    const loggedInEmail = req.session.user.email;
     User.findOne({email: loggedInEmail})
         .then(admin => {
             const username = admin.username;
@@ -36,4 +36,66 @@ exports.getAdminAccount = (req, res, next) => {
             error.httpStatusCode = 500;
             return next(error);
         })
+};
+
+exports.postAdminAccount = (req, res, next) => {
+    const newUsername = req.body.username;
+    const newEmail = req.body.email;
+    const loggedInEmail = req.session.user.email;
+    const newPassword = req.body.new_password;
+    const oldPassword = req.body.old_password;
+    const errors = validationResult(req);
+
+    User.findOne({email: loggedInEmail})
+        .then(user => {
+            const username = user.username;
+            const email = user.email;
+            if (!errors.isEmpty()) {
+                return res.status(422)
+                    .render('admin/account', {
+                        pageTitle: 'My Account',
+                        path: 'admin/account',
+                        adminDetails: {username: username, email: email},
+                        errorMessage: errors.array(),
+                        validationErrors: errors.array()
+                    });
+            }
+
+            bcrypt.compare(oldPassword, user.password)
+                .then(passwordMatch => {
+                    if (passwordMatch) {
+                        return bcrypt
+                            .hash(newPassword, 12)
+                            .then(hashedPassword => {
+                                user.password = hashedPassword;
+                                user.username = newUsername;
+                                user.email = newEmail;
+                                return user.save()
+                                    .then(result => {
+                                        res.redirect("/admin/account");
+                                    });
+                            });
+                    }
+
+                    let errors = [{param: 'old_password', msg: 'Invalid credentials'}];
+                    return res.status(422)
+                        .render('admin/account', {
+                            pageTitle: 'My Account',
+                            path: 'admin/account',
+                            adminDetails: {username: username, email: email},
+                            errorMessage: errors,
+                            validationErrors: errors
+                        });
+                })
+                .catch(err => {
+                    const error = new Error(err);
+                    error.httpStatusCode = 500;
+                    return next(error);
+                });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -13,5 +13,34 @@ const userController = require("../controllers/user");
 
 router.get('/', isAuth, isAdmin, adminController.getAdminIndex);
 router.get('/account', isAuth, isAdmin, adminController.getAdminAccount);
+router.post('/account',
+    [
+        body('username', 'Username is invalid.')
+            .isLength({min: 5})
+            .isString()
+            .trim(),
+        check('email')
+            .isEmail()
+            .withMessage('Email address is invalid.')
+            .custom((value, {req}) => {
+                return User.findOne({email: value}).then(fetchedUser => {
+                    if (fetchedUser && fetchedUser._id.toString() !== req.session.user._id.toString()) {
+                        console.log(fetchedUser._id);
+                        console.log(req.session.user._id);
+                        return Promise.reject('Email address already taken.');
+                    }
+                    ;
+                });
+            })
+            .normalizeEmail(),
+        body('new_password', 'Password is invalid.')
+            .isLength({min: 8})
+            .isAlphanumeric()
+            .withMessage('Password must be alphanumeric.')
+            .trim(),
+        body('old_password')
+            .trim()
+    ],
+    isAuth, isAdmin, adminController.postAdminAccount);
 
 module.exports = router;
