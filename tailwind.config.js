41981cd5c356685aee23603311c4fdb4025acbd7
Tenesh Vignesan <teneshdev@gmail.com>
2022-04-03 05:53:34 +0800

    Update navigation background color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         extend: {
             colors: {
                 'primary-green': '#3F6A51',
+                'header-green': '#071912',
                 'secondary-green': '#4AD19D',
                 'tertiary-green': '#e4f8f0',
                 'dark-primary-green': '#081c15',
